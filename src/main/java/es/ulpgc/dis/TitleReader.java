48028cb3fa6b4c1ba80170e5de0160eddf611544
Wafatriki <azdadwafa0@gmail.com>
2024-11-09 22:16:23 +0000

    TsvTitleDeserializer and TsvTitleReader implemented

--- src/main/java/es/ulpgc/dis/TitleReader.java
+++ src/main/java/es/ulpgc/dis/TitleReader.java
@@ -1,38 +1,7 @@
 package es.ulpgc.dis;
 
-import java.io.*;
-import java.util.ArrayList;
 import java.util.List;
 
-public class TitleReader {
-
-    private final File file;
-
-    public TitleReader(File file) {
-        this.file = file;
-    }
-
-    public List<Title> readTitle() {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("./title.basics.tsv"));
-            reader.readLine();
-            return readWith(reader);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public List<Title> readWith(BufferedReader reader) throws IOException {
-        TitleDeserializer deserializer = new TitleDeserializer();
-        List<Title> result = new ArrayList<Title>();
-        while(true) {
-            String line = reader.readLine();
-            if (line == null) break;
-            Title title = deserializer.deserialize(line);
-            result.add(title);
-
-        }
-
-        return result;
-    }
+public interface TitleReader {
+    List<Title> read();
 }
---
+++ src/main/java/es/ulpgc/dis/TsvTitleDeserializer.java
@@ -0,0 +1,47 @@
+package es.ulpgc.dis;
+
+import java.util.Arrays;
+
+public class TsvTitleDeserializer implements  TitleDeserializer {
+
+    public Title deserialize(String data) {
+        return deserialize(Arrays.toString(data.split("\t")));
+        
+        public Title deserialize() {
+            String[] fields = new String[0];
+            return new Title(fields[0], toTitleType(fields[1]), fields[2]);
+        }
+
+    }
+
+
+    private Title.Genre[] toGenreArray(String field) {
+        if (field.equals("\\N")) return new Title.Genre[0];
+        String[] split = field.split(",");
+        Title.Genre[] genres = new Title.Genre[split.length];
+        for (int i = 0; i < genres.length; i++) {
+            genres[i] = Title.Genre.valueOf(normalize(split[i]));
+        }
+        return genres;
+    }
+
+    private int toInteger(String field) {
+        if (field.equals("\\N")) return 0;
+        return Integer.parseInt(field);
+    }
+
+    private boolean toBoolean(String field) {
+        return field.equals("1");
+    }
+
+    private String normalize(String field) {
+        String upperCase = field.toUpperCase();
+        String temp = field.replace("-", "");
+        return upperCase.toCharArray()[0] + temp;
+    }
+
+    private Title.TitleType toTitleType(String field) {
+        return Title.TitleType.valueOf(normalize(field));
+
+    }
+}
